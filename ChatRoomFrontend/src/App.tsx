5444788dbbd530c2ce124395f256de78fe4347fc
Oscar Hultén <oshulten@gmail.com>
2024-08-16 11:58:19 +0200

    feat: authenticationForm

--- ChatRoomFrontend/src/App.tsx
+++ ChatRoomFrontend/src/App.tsx
@@ -1,22 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 import './App.css'
-import Login from './components/login'
-
-const fetchAllMessages = async () => {
-
-}
+import AuthenticateForm from './components/authenticateForm'
 
 export default function App() {
-  const messagesQuery = useQuery({
-    queryKey: ["messages"],
-    queryFn: fetchAllMessages
-  });
-
-
-  console.log(messagesQuery);
-
   return (
-    <Login />
-  )
+    <>
+      <AuthenticateForm submitLabel='Login' onSuccess={(fields) => console.log(fields)} />
+      <AuthenticateForm submitLabel='Create Account' onSuccess={(fields) => console.log(fields)} />
+    </>)
 }
--- ChatRoomFrontend/src/components/authenticateForm.tsx
+++ ChatRoomFrontend/src/components/authenticateForm.tsx
@@ -1,44 +1,49 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useForm, SubmitHandler } from "react-hook-form"
 
-interface AuthenticationInputs {
+interface AuthenticationFields {
     username: string
     password: string
 }
 
-export default function Login() {
-    const { register, handleSubmit, formState: { errors } } = useForm<AuthenticationInputs>({
+interface AutenticateFormProps {
+    onSuccess: (fields: AuthenticationFields) => void
+    submitLabel: string
+}
+
+export default function AuthenticateForm({ onSuccess, submitLabel }: AutenticateFormProps) {
+    const form = useForm<AuthenticationFields>({
         defaultValues: {
             username: "",
             password: ""
         }
     });
-    const onSubmit: SubmitHandler<AuthenticationInputs> = (data) => console.log(data);
 
-    console.log(errors);
+    const onSubmit: SubmitHandler<AuthenticationFields> = (fields) => onSuccess(fields);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
             <div>
                 <input
-                    {...register("username", {
+                    {...form.register("username", {
                         required: "Username is required",
                         minLength: { value: 3, message: "Username must be longer than 3 characters" }
                     })}
                     placeholder="Username"
                     className="input input-bordered w-full max-w-xs" />
-                <p>{errors["username"] ? errors.username.message : ' '}</p>
                 <input
                     type="password"
-                    {...register("password", {
+                    {...form.register("password", {
                         required: "Password is required",
                         minLength: { value: 5, message: "Password must be longer than 5 characters" }
                     })}
                     placeholder="Password"
                     className="input input-bordered w-full max-w-xs" />
-                <p>{errors.password ? errors.password.message : 'no errors'}</p>
             </div>
-            <input type="submit" />
+            <button
+                type="submit"
+                disabled={!form.formState.isValid}
+                className="btn btn-neutral">{submitLabel}</button>
         </form>
     )
 
